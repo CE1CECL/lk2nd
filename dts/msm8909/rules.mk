dfcce063c722de1cdfb6bafa4b4ae114cd3a65fd
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2023-09-08 15:33:33 -0400

    New DTBs (stock kernel)

--- dts/msm8909/rules.mk
+++ dts/msm8909/rules.mk
@@ -1,5 +1,7 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 DTBS += \
-	$(LOCAL_DIR)/msm8909-catfish.dtb \
+        $(LOCAL_DIR)/catfish_1g_ddr.dtb \
+        $(LOCAL_DIR)/catshark.dtb \
+	$(LOCAL_DIR)/catfish.dtb \
 	$(LOCAL_DIR)/msm8909-qihoo-w701.dtb
